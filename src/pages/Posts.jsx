285ef31f918bb16466d0d854205f80297b6510a8
muhammadjonr <mrahmonaliev22032007@gmail.com>
2024-04-07 01:23:32 +0500

    massage

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+// import React from 'react'
 
-const Posts = () => {
-  return (
-    <div>
-      
+// const Posts = () => {
+//   return (
+//     <div>
+//       <div className="search_studets">
+//         <input type="search" />
+//         <button>Add Students</button>
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default Posts
+
+import React from "react";
+import { Space, Table, Tag } from "antd";
+const { Column, ColumnGroup } = Table;
+const data = [
+  {
+    key: "1",
+    firstName: "John",
+    lastName: "Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    firstName: "Jim",
+    lastName: "Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
+const Posts = () => (
+  <>
+    <div className="search_studets">
+      <input type="search" />
+      <button>Add Students</button>
     </div>
-  )
-}
-
-export default Posts
+    <Table dataSource={data}>
+      <ColumnGroup title="Name">
+        <Column title="First Name" dataIndex="firstName" key="firstName" />
+        <Column title="Last Name" dataIndex="lastName" key="lastName" />
+      </ColumnGroup>
+      <Column title="Age" dataIndex="age" key="age" />
+      <Column title="Address" dataIndex="address" key="address" />
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        render={(tags) => (
+          <>
+            {tags.map((tag) => {
+              let color = tag.length > 5 ? "geekblue" : "green";
+              if (tag === "loser") {
+                color = "volcano";
+              }
+              return (
+                <Tag color={color} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </>
+        )}
+      />
+      <Column
+        title="Action"
+        key="action"
+        render={(_, record) => (
+          <Space size="middle">
+            <a>Invite {record.lastName}</a>
+            <a>Delete</a>
+          </Space>
+        )}
+      />
+    </Table>
+  </>
+);
+export default Posts;
